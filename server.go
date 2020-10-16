ea2e5c100bbb801c541f8732745e58bad102e4c6
Oleh <oleg.2014@gmail.com>
2020-10-16 22:15:52 +0300

    add submit route

--- server.go
+++ server.go
@@ -4,7 +4,7 @@ import (
 	"log"
 	"net/http"
 	"os"
-
+	
 	"github.com/gin-gonic/gin"
 )
 
@@ -15,14 +15,25 @@ func main() {
 		log.Fatal("$PORT must be set")
 	}
 
-	router := gin.New()
-	router.Use(gin.Logger())
+	router := gin.Default()
+
 	router.LoadHTMLGlob("templates/*.tmpl.html")
 	router.Static("/static", "static")
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
+	router.GET("/", indexRouter)
+	router.POST("/submit", submitRouter)
 
 	router.Run(":" + port)
+}
+
+func indexRouter(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.tmpl.html", nil)
+}
+
+func submitRouter(c *gin.Context) {
+	log.Printf(c.PostForm("url"))
+	c.JSON(http.StatusOK, gin.H{
+		"ok": true,
+		"generatedURL": "qwe",
+	})
 }
