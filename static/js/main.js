9a92785fb005da627a7c9e97700d5dcd0bd7d8da
Oleh <oleg.2014@gmail.com>
2020-10-16 22:18:09 +0300

    add functionality to send url to server

---
+++ static/js/main.js
@@ -0,0 +1,29 @@
+const userInput = document.getElementById('url_text')
+const submitButton = document.getElementById('submit_url_button')
+const errorMessage = document.getElementById('error_message')
+
+const showError = () => { errorMessage.hidden = false }
+const hideError = () => { errorMessage.hidden = true }
+
+async function sendToServer(urlText){
+  console.log('urlText =', urlText)
+  hideError()
+
+  const formBody = JSON.stringify({
+    url: urlText
+  })
+
+  const res = await window.fetch('/submit', {
+    method: 'post',
+    headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+    body: formBody
+  })
+
+  if (res.status === 200) {
+    console.log('res =', await res.json())
+  } else {
+    showError()
+  }
+}
+
+submitButton.onclick = function () { sendToServer(userInput.value) }
