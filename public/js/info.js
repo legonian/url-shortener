2f7996189f93e8bd72a4610d5a5e04eef3e15f98
Oleh <oleg.2014@gmail.com>
2020-10-18 00:10:42 +0300

    add info page ui and functionality

--- public/js/info.js
+++ public/js/info.js
@@ -1,3 +1,7 @@
+const clickCount = document.getElementById('number_of_clicks')
+const userInput = document.getElementById('url_text')
+const copyButton = document.getElementById('copy_button')
+
 async function getInfo(){
   const urlCode = window.location.pathname.split('/')[1]
   const formBody = JSON.stringify({
@@ -17,10 +21,23 @@ async function getInfo(){
   }
 }
 
+copyButton.onclick = function () {
+  userInput.select()
+  navigator.clipboard.writeText(userInput.value)
+}
+
 ; (async () => {
   const urlInfo = await getInfo()
-  if (!urlInfo){
-    return
+  if (urlInfo && urlInfo.ok){
+    clickCount.innerText = urlInfo.views_count
+
+    const hostname = window.location.hostname
+    const port = window.location.port == 80 || window.location.port == 443 ?
+      '' : `:${window.location.port}`
+    const pathname = urlInfo.short_url
+    const shortURL = `${hostname}${port}/${pathname}`
+    userInput.defaultValue = shortURL
+
+    userInput.select()
   }
-  console.log('urlInfo =', urlInfo)
 })()
