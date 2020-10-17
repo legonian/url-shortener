700cc99711b663a39335099aa0fbdfbaf9e802df
Oleh <oleg.2014@gmail.com>
2020-10-17 19:50:49 +0300

    change framework to echo; add full functionality

--- go.mod
+++ go.mod
@@ -1,5 +1,9 @@
-module github.com/legonian/url-shortener-server
+module github.com/legonian/url-shortener
 
 go 1.15
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/jackc/pgx/v4 v4.9.0
+	github.com/labstack/echo/v4 v4.1.17
+	github.com/lib/pq v1.3.0
+)
--- server.go
+++ server.go
@@ -1,12 +1,40 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"log"
 	"net/http"
-	"os"
+	"database/sql"
 	
-	"github.com/gin-gonic/gin"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	_ "github.com/lib/pq"
 )
+
+var db *sql.DB
+var err error
+
+type Data struct {
+	OK bool `json:"ok"`
+	ShortURL string `json:"short_url"`
+	FullURL string `json:"full_url"`
+	ViewsCount int `json:"views_count"`
+}
+
+func init() {
+	var err error
+	sql_url := os.Getenv("DATABASE_URL")
+	db, err = sql.Open("postgres", sql_url)
+	if err != nil {
+		panic(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		panic(err)
+	}
+	fmt.Println("You connected to your database.")
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -15,25 +43,92 @@ func main() {
 		log.Fatal("$PORT must be set")
 	}
 
-	router := gin.Default()
+	e := echo.New()
 
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
+  e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
-	router.GET("/", indexRouter)
-	router.POST("/submit", submitRouter)
+	e.Static("/public", "public")
 
-	router.Run(":" + port)
+	e.GET("/", indexRouter)
+	e.GET("/:short_url", redirectByShortURL)
+	e.GET("/:short_url/info", infoRouter)
+
+	e.POST("/submit", submitRouter)
+	e.POST("/api/:short_url", apiRouter)
+	
+	e.Logger.Fatal(e.Start(":" + port))
 }
 
-func indexRouter(c *gin.Context) {
-	c.HTML(http.StatusOK, "index.tmpl.html", nil)
+// Routes
+
+// Home Page
+func indexRouter(c echo.Context) error {
+	return c.File("public/index.html")
 }
 
-func submitRouter(c *gin.Context) {
-	log.Printf(c.PostForm("url"))
-	c.JSON(http.StatusOK, gin.H{
-		"ok": true,
-		"generatedURL": "qwe",
-	})
+// Info about short URL
+func infoRouter(c echo.Context) error {
+	return c.File("public/info.html")
+}
+
+func submitRouter(c echo.Context) error {
+	m := echo.Map{}
+	if err := c.Bind(&m); err != nil {
+		return err
+	}
+	q := fmt.Sprintf("select * from add_url('%s')", m["url"])
+	log.Print(q)
+	res := get_query(q)
+	return c.JSON(http.StatusOK, res)
+}
+
+// Redirect to full URL
+func redirectByShortURL(c echo.Context) error {
+	short_url := c.Param("short_url")
+	q := fmt.Sprintf("select * from get_full_url('%s')", short_url)
+	res := get_query(q)
+	log.Print(res.FullURL)
+	return c.Redirect(http.StatusMovedPermanently, res.FullURL) // http.StatusMovedPermanently
+}
+
+// Functions
+
+// Get raw info from database
+func get_query(query string) Data{
+	rows, err := db.Query(query)
+	if err != nil {
+		return Data{OK:false}
+	}
+	defer rows.Close()
+	res := make([]Data, 0)
+	for rows.Next() {
+		url := Data{}
+		err := rows.Scan(&url.ShortURL, &url.FullURL, &url.ViewsCount)
+		
+		if err != nil {
+			res = append(res, Data{OK:false})
+		} else {
+			url.OK = true
+			res = append(res, url)
+		}
+		
+	}
+	if err = rows.Err(); err != nil {
+		return Data{OK:false}
+	}
+	return res[0]
+}
+
+// Get info about URL
+func apiRouter(c echo.Context) error {
+	m := echo.Map{}
+	if err := c.Bind(&m); err != nil {
+		return err
+	}
+	log.Print(m)
+	q := fmt.Sprintf("select * from get_full_url('%s')", m["url"])
+	res := get_query(q)
+	log.Print(res)
+	return c.JSON(http.StatusOK, res)
 }
