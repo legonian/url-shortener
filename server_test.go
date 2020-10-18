8178f02d88af2aab2f63db4e275ef6b6b5a5117c
Oleh <oleg.2014@gmail.com>
2020-10-18 15:07:46 +0300

    adding first test to check index page access

--- server_test.go
+++ server_test.go
@@ -2,35 +2,58 @@
 package main
 
 import (
-	// "net/http"
-	// "net/http/httptest"
+	"github.com/legonian/url-shortener/database"
+	"github.com/legonian/url-shortener/handler"
+	"os"
 	"testing"
+	"net/http"
+	"net/http/httptest"
+	"database/sql"
+	"errors"
 
-	// "github.com/stretchr/testify/assert"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	_ "github.com/lib/pq"
 )
 
-//TODO
-
-//TestIndexPage
-//TestOnCreateURL
-//TestOnCreateEmpty
-//TestOnUseURL
-//TestOnWrongURL
-
 func TestIndexPage(t *testing.T) {
-	// router := indexRouter
-	// w := httptest.NewRecorder()
-	// req, _ := http.NewRequest("GET", "/ping", nil)
-	// router.ServeHTTP(w, req)
+	db, e, err := init_app()
+	if err != nil {
+		t.Fatal(err)
+	}
+	h := &handler.Handler{DB: db}
+	e.GET("/", h.Index)
 
-	// assert.Equal(t, 200, w.Code)
-	// assert.Equal(t, "pong", w.Body.String())
-	if false {
-		t.Error("Failed the test!")
+	rec := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	c := e.NewContext(req, rec)
+	err = h.Index(c)
+	if err != nil {
+		t.Fatal(err)
 	}
 }
 
-func TestOnCreateValidURL(t *testing.T) {}
-func TestOnCreateInvalidURL(t *testing.T) {}
+func TestCreatingValidURL(t *testing.T) {}
+func TestCreatingInvalidURL(t *testing.T) {}
 func TestOnGoodURL(t *testing.T) {}
 func TestOnWrongURL(t *testing.T) {}
+
+func init_app() (*sql.DB, *echo.Echo, error) {
+	db, err := database.Init()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		err = errors.New("$PORT must be set")
+		return nil, nil, err
+	}
+
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Static("/public", "public")
+
+	return db, e, err
+}
