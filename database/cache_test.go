0032f22c41d47d8bb927fa309e91f632fa82f832
Oleh <oleg.2014@gmail.com>
2020-10-22 02:59:41 +0300

    modify test to test cache limit

--- database/cache_test.go
+++ database/cache_test.go
@@ -5,32 +5,34 @@ import (
 )
 
 func TestCreateCache(t *testing.T) {
+	CACHE_LIMIT = 2
+	shortcuts := []string{"123", "456", "789"}
 	validData := Data{
 		OK:         true,
-		ShortURL:   "ShortURL",
 		FullURL:    "https://www.FullURL.com/",
-		ViewsCount: 123,
+		ViewsCount: 1111,
 	}
 	err := Init()
 	expect(t, err, nil)
 
-	err = AddCache(validData)
-	expect(t, err, nil)
-}
+	validData.ShortURL = shortcuts[0]
+	AddCache(validData)
 
-func TestGetCache(t *testing.T) {
-	validData := Data{
-		OK:         true,
-		ShortURL:   "ShortURL",
-		FullURL:    "https://www.FullURL.com/",
-		ViewsCount: 123,
-	}
-	err := Init()
-	expect(t, err, nil)
-
-	newData := CheckCache(validData.ShortURL, NotViewed)
+	newData := CheckCache(shortcuts[0], NotViewed)
 	expect(t, newData.ShortURL, validData.ShortURL)
 
-	newData = CheckCache(validData.ShortURL, IsViewed)
+	newData = CheckCache(shortcuts[0], IsViewed)
 	expect(t, newData.ViewsCount, validData.ViewsCount+1)
+
+	validData.ShortURL = shortcuts[1]
+	AddCache(validData)
+	validData.ShortURL = shortcuts[2]
+	AddCache(validData)
+
+	newData = CheckCache(shortcuts[0], IsViewed)
+	expect(t, newData.OK, false)
+	newData = CheckCache(shortcuts[1], IsViewed)
+	expect(t, newData.OK, false)
+	newData = CheckCache(shortcuts[2], IsViewed)
+	expect(t, newData.OK, true)
 }
